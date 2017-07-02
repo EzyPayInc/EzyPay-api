d02671d46a359ad73cccabf5cb81ca7f48b2a911
Diego Fonseca Marín <dfonseca@IMPESA-15.local>
2017-07-01 21:27:23 -0600

    Incluir bucket de google para almacenar imágenes de perfil de los usuarios

--- src/app/services/service.user.js
+++ src/app/services/service.user.js
@@ -1,7 +1,5 @@
 const BaseService = require("../../base/base.service").Service;
-var path = require('path');
 var EmailHandler = require("../util/Email/EmailHandler").EmailHandler;
-var fs = require('fs');
 
 class UserService extends BaseService {
 
@@ -9,24 +7,24 @@ class UserService extends BaseService {
 		return new Promise((resolve, reject) => {
 			//noinspection JSUnresolvedFunction
 			this.Models.User.create(data).then(
-				(user)=> {
-					if(data.tablesQuantity <= 0) {
-                        resolve(user.id);
+				(user) => {
+					if (data.tablesQuantity <= 0) {
+						resolve(user.id);
 					} else {
-						 this.insertTables(user.id, data.tablesQuantity).then(
-							 (result)=> resolve(user.id),
-							 (error)=> reject(error)
-						 );
+						this.insertTables(user.id, data.tablesQuantity).then(
+							(result) => resolve(user.id),
+							(error) => reject(error)
+						);
 					}
 				},
-				(error)=> reject(error)
+				(error) => reject(error)
 			);
 		});
 	}
 
 	getAll(criteria) {
 		//noinspection JSUnresolvedFunction
-		return this.Models.User.findAll({where: criteria});
+		return this.Models.User.findAll({ where: criteria });
 	}
 
 	getById(id) {
@@ -35,59 +33,30 @@ class UserService extends BaseService {
 	}
 
 	updateById(id, data) {
-		return this.Models.User.update(data, {where: {"id": id}});
+		return this.Models.User.update(data, { where: { "id": id } });
 	}
 
 	validateAccount(id) {
 		return this.Models.User.update({
 			isValidatedAccount: 1
-		}, {where: {"id": id}});
+		}, { where: { "id": id } });
 	}
 
 	validatePhoneNumbers(phoneNumbers) {
-        return this.Models.User.findAll({
-        	where: { phoneNumber: {$in: phoneNumbers}}
-        });
+		return this.Models.User.findAll({
+			where: { phoneNumber: { $in: phoneNumbers } }
+		});
 	}
 
-	updateUserImage(id, filename) {
+	uploadUserImage() {
 		return this.Models.User.update({
-			avatar : filename
-		}, {where: {"id": id}});
-	}
-
-   uploadUserImage() {
-        return new Promise((resolve, reject)=> {
-            this.upload(true).then((data)=> {
-				var filename = "upload_" + data[0].path.split("_")[1];
-				this.updateUserImage(this.user.id, filename).then((result) => {
-					resolve(result);
-				}, (error)=> reject(error));
-            }, (error)=> reject(error));
-        });
-    }
-
-	getUserProfileImage(id)
-	{
-        return new Promise((resolve, reject) => {
-            this.getById(id).then(
-                (user)=> {
-                	var file = this.getFile(user.avatar);
-                	if(file == null) {
-                		reject("Not found");
-					} else {
-                        resolve(file);
-					}
-                },
-                (error)=> {
-                	reject(error);
-                }
-            );
-        });
+			avatar: this.req.file.csObject
+		}, { where: { "id": this.user.id } });
 	}
 
 	insertTables(commerceId, tablesQuantity) {
-        return this.DBs[0].query('CALL sp_insertTables('+commerceId+', '+tablesQuantity+');');
+		return this.DBs[0].query('CALL sp_insertTables('
+			+ commerceId + ', ' + tablesQuantity + ');');
 	}
 }
 module.exports = UserService;
--- src/routers/router.user.js
+++ src/routers/router.user.js
@@ -1,23 +1,31 @@
-var express = require("express");
-// var authenticated = require("../config/policies/authenticated");
 var UserController = require("../app/controllers").UserController;
+var GCloudStorage = require("../app/services").GCloudStorage;
 var policies = require("../policies");
+var config = require("../config");
+var express = require("express");
 
 var router = express.Router();
-//noinspection JSUnresolvedFunction
-router.post("/", UserController.create);
-//noinspection JSUnresolvedFunction
-router.post("/getAll", policies.BearerAuth, UserController.getAll);
-//noinspection JSUnresolvedFunction
-router.get("/:id", policies.BearerAuth, UserController.getById);
-//noinspection JSUnresolvedFunction
-router.put('/:id', policies.BearerAuth, UserController.updateById);
-//noinspection JSUnresolvedFunction
-router.get('/validate/:id', policies.BearerAuth, UserController.validateAccount);
-//noinspection JSUnresolvedFunction
-router.post('/validatePhoneNumbers', policies.BearerAuth, UserController.validatePhoneNumbers);
-//noinspection JSUnresolvedFunction
-router.post('/uploadImage/:id', policies.BearerAuth, UserController.uploadUserImage);
-router.get('/downloadImage/:id', UserController.downloadUserImage);
+router.post("/",
+    UserController.create);
+router.post("/getAll",
+    policies.BearerAuth,
+    UserController.getAll);
+router.get("/:id",
+    policies.BearerAuth,
+    UserController.getById);
+router.put('/:id',
+    policies.BearerAuth,
+    UserController.updateById);
+router.get('/validate/:id',
+    policies.BearerAuth,
+    UserController.validateAccount);
+router.post('/validatePhoneNumbers',
+    policies.BearerAuth,
+    UserController.validatePhoneNumbers);
+router.post('/uploadImage/:id',
+    policies.BearerAuth,
+    config.multer.single('image'),
+    GCloudStorage.sendUploadToGCS,
+    UserController.uploadUserImage);
 
 module.exports = router;
